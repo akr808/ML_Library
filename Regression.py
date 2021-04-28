62ce161903dcba3c56ee835455eba17a5859999d
akr808 <akr808@gmail.com>
2021-04-28 14:33:35 +0530

    Ml Preprocessing
    Ml Preprocessing steps

---
+++ Regression.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+ML Lab for the udemy course "Machine Learning A-Z (Codes and Datasets)" 
+
+@author: Anand
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+dataset = pd.read_csv("Data/Data.csv")
