b397da3bc295eb5cf451d50a6d17a7a16867045e
akr808 <akr808@gmail.com>
2021-05-19 14:45:03 +0530

    Update dimensionality_reduction_pca.py

--- Python/dimensionality_reduction_pca.py
+++ Python/dimensionality_reduction_pca.py
@@ -27,7 +27,7 @@ y = dataset.iloc[:, -1]
 
 
 #Split the data into train & test set
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0)
 
 #Feature Scaling#
 sc = StandardScaler()
