58c476b18e7556de97100321cdda94aa74f5d906
akr808 <akr808@gmail.com>
2021-05-20 16:44:16 +0530

    Create association_rule_mining_apriori.R

---
+++ R/association_rule_mining_apriori.R
@@ -0,0 +1,17 @@
+#' 
+#' ML Lab for the udemy course "Machine Learning A-Z (Codes and Datasets)"
+#' Topic: Association Rule Mining - Apriori
+#' @author: Anand
+
+
+#Import the dataset#
+library(arules)
+#Create the sparse matrix#
+dataset <- read.transactions("../Data/Market_Basket_Optimisation.csv",sep = ',', rm.duplicates = TRUE)
+
+#Frequency PLot for the transaction
+itemFrequencyPlot(dataset, topN = 100)
+
+
+#Building the apriori model
+rules <- apriori(data = dataset, parameter = list(support = 0.003,confidence = 0.6, minlen = 2) )
