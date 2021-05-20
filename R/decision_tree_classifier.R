553a0f14b0cefc51f1765557b075344928d40840
akr808 <akr808@gmail.com>
2021-05-20 14:43:44 +0530

    Create decision_tree_classifier.R

---
+++ R/decision_tree_classifier.R
@@ -0,0 +1,68 @@
+#' 
+#' ML Lab for the udemy course "Machine Learning A-Z (Codes and Datasets)"
+#' Topic: Decision Tree Classifier
+#' @author: Anand
+
+#Import the dataset#
+dataset <- read.csv("../Data/Social_Network_Ads.csv")
+
+#Splitting the data into train and test#
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = .75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#Feature Scaling#
+training_set[,1:2] = scale(training_set[,1:2])
+test_set[,1:2] = scale(test_set[,1:2])
+
+#Training the model
+set.seed(1234)
+library(rpart)
+classifier = rpart(formula = Purchased ~ ., data = training_set)
+
+#Predicting the probabilities
+y_pred = predict(classifier, newdata = test_set[-3])
+y_pred = ifelse(y_pred > 0.5, 1, 0)
+
+#Computiing the model characteristics#
+cm = table(test_set[,3], y_pred)
+
+#Visualising the results#
+# Visualising the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+y_grid = ifelse(y_grid > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Decision Tree Classifier (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'Decision Tree Classifier (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Plotting the tree
+plot(classifier)
+text(classifier)
